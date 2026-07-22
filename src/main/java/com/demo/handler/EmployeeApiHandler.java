ebfb8de7c57ed12cf50d69ba6f14f0029aa79cc0
Meet10-tatvasoft <meet.dadhaniya@etatvasoft.com>
2026-07-22 16:02:23 +0530

    Removed DBConnection , added template.yml, buildspec.yml, openai.json

--- src/main/java/com/demo/handler/EmployeeApiHandler.java
+++ src/main/java/com/demo/handler/EmployeeApiHandler.java
@@ -6,7 +6,6 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.demo.dao.EmployeeDAO;
-import com.demo.model.Employee;
 import com.demo.util.JsonUtil;
 
 import java.util.Map;
@@ -16,59 +15,39 @@ public class EmployeeApiHandler implements RequestHandler<APIGatewayProxyRequest
     private final EmployeeDAO employeeDAO = new EmployeeDAO();
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request,Context context) {
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+
         try {
+
             LambdaLogger logger = context.getLogger();
             logger.log(request.toString());
+
             String method = request.getHttpMethod();
             String path = request.getPath();
 
             switch (method) {
-                case "GET":
-                    if ("/employees".equals(path)) {
-                        return response(200,JsonUtil.toJson(employeeDAO.getAllEmployees()));
-                    }
-                    if (path.startsWith("/employees/")) {
 
-                        int id = Integer.parseInt(request.getPathParameters().get("id"));
-                        Employee employee = employeeDAO.getEmployeeById(id);
-                        if (employee == null) {
-                            return response(404, "Employee Not Found");
-                        }
-                        return response(200,JsonUtil.toJson(employee));
+                case "GET":
+                    if ("/employees".equals(path) || path.startsWith("/employees/")) {
+                        return response(200, JsonUtil.toJson(employeeDAO.getAllEmployees()));
                     }
                     break;
+
                 case "POST":
                     if ("/employees".equals(path)) {
-                        Employee employee = JsonUtil.fromJson(request.getBody(), Employee.class);
-                        employeeDAO.addEmployee(employee);
-                        return response(201, "Employee Created");
+                        return response(200, JsonUtil.toJson(employeeDAO.getAllEmployees()));
                     }
                     break;
+
                 case "PUT":
                     if (path.startsWith("/employees/")) {
-                        int id = Integer.parseInt(request.getPathParameters().get("id"));
-                        Employee employee = JsonUtil.fromJson(request.getBody(), Employee.class);
-                        boolean isUpdated = employeeDAO.updateEmployee(id, employee);
-                        if (isUpdated){
-                            return response(200, "Employee Updated");
-                        }
-                        else{
-                            return response(200, "Employee Not Found With this ID");
-                        }
-
+                        return response(200, JsonUtil.toJson(employeeDAO.getAllEmployees()));
                     }
                     break;
+
                 case "DELETE":
                     if (path.startsWith("/employees/")) {
-                        int id = Integer.parseInt(request.getPathParameters().get("id"));
-                        boolean isDeleted = employeeDAO.deleteEmployee(id);
-                        if (isDeleted){
-                            return response(200, "Employee Deleted");
-                        }
-                        else{
-                            return response(200, "Employee Not Found With this ID");
-                        }
+                        return response(200, JsonUtil.toJson(employeeDAO.getAllEmployees()));
                     }
                     break;
             }
