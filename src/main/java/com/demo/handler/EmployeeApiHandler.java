a6fe92b9c6f633b9d335b30b61422348d42f1226
Meet10-tatvasoft <meet.dadhaniya@etatvasoft.com>
2026-07-30 17:28:40 +0530

    Changing prd to version 2 and adding demo-api-5 to move dev to version 3

--- src/main/java/com/demo/handler/EmployeeApiHandler.java
+++ src/main/java/com/demo/handler/EmployeeApiHandler.java
@@ -42,6 +42,9 @@ public class EmployeeApiHandler implements RequestHandler<APIGatewayProxyRequest
                     }if("/demo-api-4".equals(path)){
                         return response(200, JsonUtil.toJson(employeeDAO.getAllEmployees()));
                     }
+                    if("/demo-api-5".equals(path)){
+                        return response(200, JsonUtil.toJson(employeeDAO.getAllEmployees()));
+                    }
                     break;
 
                 case "POST":
